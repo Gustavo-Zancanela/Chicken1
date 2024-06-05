3d372c2c70a445429bc1153563a45846bd19e18e
ALUNO <ALUNO@LAB04-PC32>
2024-06-05 20:22:07 -0300

    Commit project Chicken1

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -4,55 +4,63 @@ public class Chickens01 {
     int eggsPerChicken2 = 4;
     int chickenCount2 = 8;
     
-    public int Segunda()
+    public int Segunda() //calcula os ganhos de segunda
     {
          int segunda = eggsPerChicken * chickenCount;
          return segunda;
     }
     
-    public int Terca()
+    public int Terca()  //calcula os ganhos de terca
     {
         chickenCount++;
         int terca =  eggsPerChicken * chickenCount;
         return terca;
     }
     
-    public int Quarta()
+    public int Quarta()  //calcula os ganhos de quarta
     {
         int newChicken = chickenCount / 2;
         int quarta =  newChicken * eggsPerChicken;
         return quarta;
     }
     
-     public int Segunda2()
+     public int Segunda2()//calcula os ganhos de segunda 2
     {
          int segunda2 = eggsPerChicken2 * chickenCount2;
          return segunda2;
     }
     
-    public int Terca2()
+    public int Terca2() //calcula os ganhos de terca 2
     {
         chickenCount2++;
         int terca2 =  eggsPerChicken2 * chickenCount2;
         return terca2;
     }
     
-    public int Quarta2()
+    public int Quarta2()//calcula os ganhos de quarta 2
     {
         int newChicken2 = chickenCount2 / 2;
-        int quarta =  newChicken2 * eggsPerChicken2;
+        int quarta2 =  newChicken2 * eggsPerChicken2;
         return quarta2;
     }
     
     
     public static void main(String[] args) {
        
-        Chickens01 chicken = new Chickens01();
+        Chickens01 chicken = new Chickens01(); //recebe os dados
         int segunda =  chicken.Segunda();
         int terca =  chicken.Terca();
         int quarta = chicken.Quarta();
         
-        int totalEggs = segunda + terca + quarta;
-        System.out.println(totalEggs);
+        int segunda2 =  chicken.Segunda2();//recebe os dados
+        int terca2 =  chicken.Terca2();
+        int quarta2 = chicken.Quarta2();
+        
+        int totalEggs = segunda + terca + quarta; //soma o total
+         int totalEggs2 = segunda2 + terca2 + quarta2;//soma o total
+         
+        System.out.println(totalEggs); //mostra o total
+        System.out.println(totalEggs2); //mostra o total
+
     }   
 }
